1b61fb7f8644e935f12c3f6e2be374baf01bb3aa
Oluwanifise Oguntoye <117686447+oluwanifiseoguntoye@users.noreply.github.com>
2023-08-14 16:00:19 -0400

    Slight edits

--- clients/src/pages/OrderComplete.js
+++ clients/src/pages/OrderComplete.js
@@ -12,7 +12,9 @@ function OrderComplete() {
       <h2>Order placed!</h2>
       <h1 className="orderID">Order ID: {orderId}</h1>
       <p className="closingInfo">
-        *To complete your purchase, please DM your Order ID to our Instagram Account
+        *To complete your purchase, please DM your Order ID to our Instagram Account. 
+        <br></br>Your total cost will be calculated and you're expected to pay the total through 
+        e-transfer in 24 hours or your order will be cancelled
       </p>
     </div>
   );
